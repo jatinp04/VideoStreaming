068dcd79b393a14d68d1c80d79c3e999a5c182e3
Jatin <jatinpatel292@gmail.com>
2023-03-04 02:07:06 +0530

    MenuComp Completed

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import styled from "styled-components";
+import thumbnail from "../img/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+const Container = styled.div`
+  flex: 1;
+  background-color: #1e1e1e;
+  height: 100vh;
+  color: white;
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+`;
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`;
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.soft};
+  }
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid white;
+`;
+
+const Login = styled.div``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
+`;
+
+const Menu = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={thumbnail} />
+          VideoTube
+        </Logo>
+
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+
+        <Hr />
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlinedIcon />
+            SIGN IN
+          </Button>
+        </Login>
+
+        <Hr />
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Menu;
