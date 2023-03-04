02c1097c319fc083e37f57a4c97c3f1499bee26a
Jatin <jatinpatel292@gmail.com>
2023-03-05 02:02:59 +0530

    router configured

---
+++ server/routes/videos.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { addVideo, addView, getVideo, search } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+//create a video
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, addVideo)
+router.delete("/:id", verifyToken, addVideo)
+router.get("/find/:id", getVideo)
+router.put("/view/:id", addView)
+router.get("/search", search)
+
+export default router;
